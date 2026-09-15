fd16d4563dcf02333d8c1e3c0303e316d1a9b4ec
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-15 16:39:52 +0530

    [Syncode] GeeksForGeeks/Easy/Reverse Array

---
+++ GeeksForGeeks/Easy/Reverse Array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int l = 0;
+        int r = arr.size()-1;
+        
+        while(l<=r){
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+};
