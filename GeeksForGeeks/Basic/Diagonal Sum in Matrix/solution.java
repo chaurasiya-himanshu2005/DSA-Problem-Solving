22d7968c11f2d36094778212938da8d9d5840829
Himanshu Chaurasiya <chaurasiya.himanshu2005@gmail.com>
2026-09-19 18:52:09 +0530

    [Syncode] GeeksForGeeks/Basic/Diagonal Sum in Matrix

---
+++ GeeksForGeeks/Basic/Diagonal Sum in Matrix/solution.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        // code here
+        int sum = 0;
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                if(i==j){
+                    sum += mat[i][j];
+                }
+            }
+        }
+        int j=mat.length-1;
+        for(int i=0;i<mat.length;i++){
+            sum+=mat[i][j];
+            j--;
+        }
+        return sum;
+    }
+}
